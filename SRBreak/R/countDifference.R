1f6a2395fdba39d4d5844c506b84796f7a4ae951
hoangtn <hoangtannguyenvn@gmail.com>
2015-05-29 20:53:31 +1200

    Update

--- SRBreak/R/countDifference.R
+++ SRBreak/R/countDifference.R
@@ -1,16 +1,22 @@
-calculateVst <- function(data, popName, regions = NULL){
+##This function is used to count differences between populations
+countDifference <- function(data, popName, regions = NULL,
+                         upperThreshold = 0.2,
+                         lowerThreshold = -0.2){
 
     ##data is a data.frame/matrix of segmentation results
     ##popName is a data.frame including two columns: sample names and their corresponding populations
 
     
-    
     popName <- popName[pmatch(rownames(data), popName[, 1]), ]
+
     ###Calculate number of populations
     nPops <- length(table(popName[, 2]))
  
 
     Vst <- apply(data, 2, function(x){
+
+        x <- ifelse((x > lowerThreshold) & (x < upperThreshold), 0, 1)
+                 
           bTemp <- data.frame(data = x,
                               pop = as.character(popName[, 2])) ##Combine a column and population information
           
@@ -22,16 +28,12 @@ calculateVst <- function(data, popName, regions = NULL){
               for (jST in (iST+1):nPops){ #jST for the second pop
                   bTempIJ <- bTemp[(bTemp[, 2] == popNames[iST]) |
                              (bTemp[, 2] == popNames[jST]), ] #Extract the two pops
-                  bVT <- sd(bTempIJ[, 1]) #Total variance across individuals across two populations
+                  
 
-                  bVSTtemp <- sapply(split(bTempIJ, bTempIJ$pop), function(x) sd(x[, 1])) ##Calculate each variance of each pop
+                  bVSTtemp <- sapply(split(bTempIJ, bTempIJ$pop),
+                                     function(x) dim(x[x[, 1] > 0, ])[1])
 
-                  bVSTtemp <- data.frame(bVSTtemp, table(popAll)) ##Combine variances and pops
-                  bVSTtemp <- bVSTtemp[!is.na(bVSTtemp[, 1]), ] ##Remove NA
-
-
-                  bVS <- sum(bVSTtemp[, 1]*bVSTtemp[, 3])/sum(bVSTtemp[, 3]) ##Calcualte the average within-population variance Vs = (V1*n1 + V2*n2)/(n1 + n2)
-                  vResults[indexVST] <- (bVT - bVS)/bVT ##Vst = (Vt - Vs)/Vt
+                  vResults[indexVST] <- abs(bVSTtemp[1] - bVSTtemp[2])
 
                   nameValueVST[indexVST] <- paste(popNames[iST], "-", popNames[jST], sep = "")
                   indexVST <- indexVST + 1
